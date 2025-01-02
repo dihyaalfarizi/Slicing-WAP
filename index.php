8a7c5dded9e933a1f3ab5a51b1179d2cee6e8609
dihyaalfarizi <dihyalfarizi@gmail.com>
2025-01-02 23:47:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('head.php') ?>
+</head>
+
+<body>
+    <?php include('header.php') ?>
+    <?php include('navbar.php') ?>
+    <?php include('content.php') ?>
+    <?php include('footer.php') ?>
+</body>
+
+</html>
